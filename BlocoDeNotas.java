81891af1695fef77af54f9a39cba1ce4cc41e167
YvesPereira21 <128404191+YvesPereira21@users.noreply.github.com>
2023-08-28 14:29:22 -0300

    Update BlocoDeNotas.java

--- BlocoDeNotas.java
+++ BlocoDeNotas.java
@@ -6,12 +6,8 @@ public class BlocoDeNota {
 
     Scanner sc = new Scanner(System.in);
     List<Anotacao> Anotacoes= new ArrayList();
+    List<Anotacao> notasDeletadas = new ArrayList<>();
     //adicionar, editar, recuperar com id ou texto, deletar e listar.
-    public Anotacao getAnotacaoId(int id){
-        System.out.println("id que deseja pegar: ");
-        Anotacao pegaId = Anotacoes.get(id - 1);
-        return pegaId;
-    }
 
 
     public void adiciona(String textao){
@@ -20,28 +16,52 @@ public class BlocoDeNota {
     }
 
     public void editarNota(int id, String textoEditado){
-        Anotacao notaEditada = getAnotacaoId(id);
+        Anotacao notaEditada = buscarNota(id);
         notaEditada.setTextao(textoEditado);
     }
-    public void recuperarNota(int id){
-        System.out.println("Qual o id da anotação que deseja recuperar? ");
-        int anotacaoEditar = Anotacoes.indexOf(sc.nextInt());
-        String texto = sc.next();
-        Anotacoes.get(anotacaoEditar);
+    public Anotacao buscarNota(int id){
+        return Anotacoes.get(id - 1);
     }
 
-    public void recuperarNota(String buscoTexto){
+    public String pesquisarAluno(String textoBuscar){
+        String textos = "";
+        for (Anotacao anota: Anotacoes) {
+            if(anota.getTextao().contains(textoBuscar)){
+                textos += (anota.toString());
+            }
+        }
+        return textos;
+    }
 
-
-        for (int i = 0; i < Anotacoes.size(); i++) {
-            Anotacao an = Anotacoes.get(i);
-            buscoTexto = sc.next();
-            if (an.getTextao().contains(buscoTexto)){
-                System.out.println(an.toString());
+    public void deletaNota(int id){
+        for (Anotacao notaDeletar: Anotacoes) {
+            if(notaDeletar.getId() == id - 1){
+                Anotacao a = Anotacoes.get(id - 1);
+                notasDeletadas.add(a);
+                Anotacoes.remove(id - 1);
             }
         }
     }
-    public void setAnotacoes(List<Anotacao> anotacoes) {
-        Anotacoes = anotacoes;
+
+    public void recuperaNota(int id){
+        for (Anotacao notaDeletada: notasDeletadas) {
+            if(notaDeletada.getId() == id){
+                Anotacao a = Anotacoes.get(id - 1);
+                Anotacoes.add(a);
+                notasDeletadas.remove(id - 1);
+            }
+        }
     }
+
+    @Override
+    public String toString(){
+
+        String todasAnotacoes = "";
+        for (Anotacao aluno: Anotacoes) {
+            todasAnotacoes += (aluno.toString());
+        }
+        return todasAnotacoes;
+    }
+
+
 }
